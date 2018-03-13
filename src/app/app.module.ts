1f8972de23439f2a2469ea2fc339ca12d3359d12
Darron Stone <darron.stone@thetestfactory.com>
2018-03-13 16:23:05 +0000

    first commit on angular fundamentals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeroListComponent } from './components/hero-list/hero-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule
